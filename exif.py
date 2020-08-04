d2e6134b645b4b62ef63b5ce6c0b44f8689a9ba7
mateusjdev <mateusjuniordev@gmail.com>
2020-08-04 18:34:43 -0300

    fix some things and updated README.md Removed compiled .exe for use only in .py Orientation now remains the same as the input Added .jpeg files Output will be converted to RGB

--- exif.py
+++ exif.py
@@ -1,28 +1,40 @@
-from PIL import Image
+from PIL import Image, ExifTags
 import os
 #exif remove dir list
-os.system('cls')
-os.system('title JPG/PNG Exif Remove ( CLI UI ) v.1.2 ~ @root_haxor')
-print "\n\nInitiating exif data removing sequence...\n"
+print("Initiating exif data removing sequence...\n")
 relevant_path = os.getcwd()
-extenstions = ['jpg', 'png']
-listfiles = [files for files in os.listdir(relevant_path)
-          	if any(files.endswith(ext) for ext in extenstions)]
+extenstions = ['jpg', 'png' ,'jpeg']
+listfiles = [files for files in os.listdir(relevant_path) if any(files.endswith(ext) for ext in extenstions)]
 #inbuilt function
 #content = filter(lambda x: not x.startswith('ex_'), listfiles)
 newlist = [x for x in listfiles if not x.startswith('ex_')]
 if len(newlist) == 0:
-	os.system('cls')
-	print "\n\nError! Either Exif data is already removed or No jpg/png files found"
+    os.system('cls')
+    print("Error! Either Exif data is already removed or No jpg/png files found")
 for image_file in newlist:
-	image  = Image.open(image_file)
-	data = list(image.getdata())
-	image_without_exif = Image.new(image.mode, image.size)
-	image_without_exif.putdata(data)
-	directory = os.getcwd()
-	image_without_exif.save(directory + "/ex_" + image_file)
-	print "Exif Removed, saved as \ex_%s" % (image_file)
-	delete = os.remove(image_file)
-print "\n\n+-----------------------------------+"
-print "  Exif Removed from %s file or files"%len(newlist)
-print "+-----------------------------------+"
+    image = Image.open(image_file)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    data = list(image.getdata())
+    image_without_exif = Image.new(image.mode, image.size)
+    image_without_exif.putdata(data)
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation] == 'Orientation':
+            break
+    pre_exif = image.getexif()
+    if pre_exif is not None:
+        exif = dict(pre_exif.items())
+        if len(exif) is not 0:
+            if orientation in exif:
+                exif_or = exif[orientation]
+                if exif_or == 3:
+                    image_without_exif = image_without_exif.rotate(180, expand=True)
+                elif exif_or == 6:
+                    image_without_exif = image_without_exif.rotate(270, expand=True)
+                elif exif_or == 8:
+                    image_without_exif = image_without_exif.rotate(90, expand=True)
+    directory = os.getcwd()
+    image_without_exif.save(directory + "/ex_" + image_file)
+    print("(Removed) Saved as .\ex_" + image_file)
+    delete = os.remove(image_file)
+x = input("Press any key to exit...")
